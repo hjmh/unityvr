17f2d7fc2515a1fb5e196091cc104e51ac4d0810
hjmh <hjmhaberkern@gmail.com>
2021-01-13 13:34:52 -0500

    plotting trajectories in vr and object positions

---
+++ unityvr/viz/viz.py
@@ -0,0 +1,32 @@
+### This module contains functions for plotting VR data, including functions to vizualize trajectories, frame rate, ...
+
+import matplotlib.patches as mpatches
+from matplotlib import patches
+import matplotlib.pyplot as plt
+
+def plotVRpathWithObjects(uvrExperiment,limx,limy, myfigsize):
+
+    fig, ax = plt.subplots(1,1, figsize=myfigsize)
+    
+    ax = plotAllObjects(uvrExperiment, ax)
+
+    ax.plot(uvrExperiment.posDf['x'], uvrExperiment.posDf['y'],color='grey',alpha=0.5)
+    ax.scatter(uvrExperiment.posDf['x'], uvrExperiment.posDf['y'],s=7,c=uvrExperiment.posDf['time'],cmap='viridis')
+    ax.set_ylim(limx[0], limx[1])
+    ax.set_xlim(limy[0], limy[1])
+    ax.set_aspect('equal')
+    
+    return fig
+
+def plotAllObjects(uvrExperiment, ax):
+    
+    for obj in range(uvrExperiment.objDf.shape[0]):
+        if("FlyCamera" not in objDf.name[obj]):
+            #print(objDf['name'][obj])
+            ellipse = mpatches.Ellipse((uvrExperiment.objDf['px'][obj], 
+                                        uvrExperiment.objDf['py'][obj]), 
+                                        uvrExperiment.objDf['sx'][obj], 
+                                        uvrExperiment.objDf['sy'][obj], color='grey', alpha=0.5)
+            ax.add_patch(ellipse)
+            ax.annotate(uvrExperiment.objDf['name'][obj], (uvrExperiment.objDf['px'][obj]+5, objDf['py'][obj]-10))
+    return ax
