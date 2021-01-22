4a76afc969c148089d8f90fe9cc4ab6a44073844
hjmh <hjmhaberkern@gmail.com>
2021-01-22 17:12:36 -0500

    added time interpolation, clean up

--- unityvr/preproc/logproc.py
+++ unityvr/preproc/logproc.py
@@ -1,6 +1,7 @@
 ### This module contains basic preprocessing functions for processing the unity VR log file
 
 import pandas as pd
+import numpy as np
 from dataclasses import dataclass, asdict
 from os import mkdir, makedirs
 from os.path import sep, isfile, exists
@@ -12,7 +13,7 @@ objDfCols = ['name','collider','px','py','pz','rx','ry','rz','sx','sy','sz']
 posDfCols = ['frame','time','x','y','angle']
 ftDfCols = ['frame','ficTracTReadMs','ficTracTWriteMs','dx','dy','dz']
 dtDfCols = ['frame','time','dt']
-pdDfCols = ['frame','time','pdsig']
+nidDfCols = ['frame','time','dt','pdsig']
 
 # Data class definition
 
@@ -28,6 +29,7 @@ class unityVRexperiment:
     # timeseries data
     posDf: pd.DataFrame = pd.DataFrame(columns=posDfCols)
     ftDf: pd.DataFrame = pd.DataFrame(columns=ftDfCols)
+    nidDf: pd.DataFrame = pd.DataFrame(columns=nidDfCols)
         
     # object locations
     objDf: pd.DataFrame = pd.DataFrame(columns=objDfCols)
@@ -64,6 +66,7 @@ class unityVRexperiment:
         self.objDf.to_csv(sep.join([saveDir,saveName,'objDf.csv']))
         self.posDf.to_csv(sep.join([saveDir,saveName,'posDf.csv']))
         self.ftDf.to_csv(sep.join([saveDir,saveName,'ftDf.csv']))
+        self.nidDf.to_csv(sep.join([saveDir,saveName,'nidDf.csv']))
 
         
 # constructor for unityVRexperiment
@@ -73,50 +76,40 @@ def constructUnityVRexperiment(dirName,fileName):
     
     metadat = makeMetaDict(dat, fileName)
     objDf = objDfFromLog(dat)
-    posDf, ftDf, tsDf = timeseriesDfFromLog(dat)
+    posDf, ftDf, nidDf = timeseriesDfFromLog(dat)
 
-    uvrexperiment = unityVRexperiment(metadata=metadat,posDf=posDf,ftDf=ftDf,objDf=objDf)
+    uvrexperiment = unityVRexperiment(metadata=metadat,posDf=posDf,ftDf=ftDf,nidDf=nidDf,objDf=objDf)
     
     return uvrexperiment
 
 
-def parseHeader(notes):
-    headerwords = ["expid", "experiment", "genotype","flyid","sex","\n"]
-    metadat = ['testExp', 'test experiment', 'testGenotype', 'NA', 'NA']
+def parseHeader(notes, headerwords, metadat):
     
     for i, hw in enumerate(headerwords[:-1]):
         if hw in notes:
             metadat[i] = notes[notes.find(hw)+len(hw)+1:notes.find(headerwords[i+1])].split('-')[0]
-    expid=metadat[0].strip()
-    exp=metadat[1].strip()
-    gt=metadat[2].strip()
-    fi=metadat[3].strip()
-    sx=metadat[4].strip()
-    
-    return expid, exp, gt, fi, sx
+            
+    return metadat
 
 def makeMetaDict(dat, fileName):
+    headerwords = ["expid", "experiment", "genotype","flyid","sex","notes","\n"]
+    metadat = ['testExp', 'test experiment', 'testGenotype', 'NA', 'NA', "NA"]
     
     if 'headerNotes' in dat[0].keys():
         headerNotes = dat[0]['headerNotes']
-        [expid, exp, gt, fi ,sx] = parseHeader(headerNotes)
-    else:
-        expid = 'testExp'
-        exp = 'test experiment'
-        gt = 'testGenotype'
-        fi = 'NA'
-        sx = 'NA'
+        metadat = parseHeader(headerNotes, headerwords, metadat)
 
     [datestr, timestr] = fileName.split('.')[0].split('_')[1:]
     
     metadata = {
-        'expid': expid,
-        'experiment': exp,
-        'genotype': gt,
-        'sex': sx,
-        'flyid': fi,
+        'expid': metadat[0].strip(),
+        'experiment': metadat[1].strip(),
+        'genotype': metadat[2].strip(),
+        'sex': metadat[4].strip(),
+        'flyid': metadat[3].strip(),
         'date': datestr,
-        'time': timestr
+        'time': timestr,
+        'notes': metadat[5].strip()
     }
     
     return metadata
@@ -165,11 +158,13 @@ def objDfFromLog(dat):
             
     return objDf
 
-def timeseriesDfFromLog(dat):    
+def timeseriesDfFromLog(dat):
+    from scipy.signal import medfilt
+
     posDf = pd.DataFrame(columns=posDfCols)
     ftDf = pd.DataFrame(columns=ftDfCols)
     dtDf = pd.DataFrame(columns=dtDfCols)
-    pdDf = pd.DataFrame(columns=pdDfCols)
+    pdDf = pd.DataFrame(columns = ['frame','time','pdsig'])
     # add 3rd timeseries with deltaTime? Or merge onto one?
 
     nlines = sum(1 for line in dat)
@@ -212,12 +207,48 @@ def timeseriesDfFromLog(dat):
             pdDf = pdDf.append(framedat, ignore_index = True)
             
     posDf.time = posDf.time-posDf.time[0]
-    dtDf.time = posDf.time-dtDf.time[0]
+    dtDf.time = dtDf.time-dtDf.time[0]
     pdDf.time = pdDf.time-pdDf.time[0]
     
     ftDf.ficTracTReadMs = ftDf.ficTracTReadMs-ftDf.ficTracTReadMs[0]
     ftDf.ficTracTWriteMs = ftDf.ficTracTWriteMs-ftDf.ficTracTWriteMs[0]
     
-    tsDf = pd.merge(dtDf, pdDf, on="frame", how='outer').rename(columns={'time_x':'time'}).drop(['time_y'],axis=1)
+    posDf = pd.merge(dtDf, posDf, on="frame", how='outer').rename(columns={'time_x':'time'}).drop(['time_y'],axis=1)
+    nidDf = pd.merge(dtDf, pdDf, on="frame", how='outer').rename(columns={'time_x':'time'}).drop(['time_y'],axis=1)
+
     
-    return posDf, ftDf, tsDf
+    nidDf["pdFilt"]  = nidDf.pdsig.values
+    nidDf.pdFilt[np.isfinite(nidDf.pdsig)] = medfilt(nidDf.pdsig[np.isfinite(nidDf.pdsig)])
+    nidDf["pdThresh"]  = 1*(np.asarray(nidDf.pdFilt>=np.nanmedian(nidDf.pdFilt.values)))
+    
+    nidDf = generateInterTime(nidDf)
+    
+    return posDf, ftDf, nidDf
+
+
+def generateInterTime(tsDf):
+    from scipy import interpolate
+    
+    tsDf['framestart'] = np.hstack([0,1*np.diff(tsDf.time)>0])
+
+    tsDf['counts'] = 1
+    sampperframe = tsDf.groupby('frame').sum()[['time','dt','counts']].reset_index(level=0)
+    sampperframe['fs'] = sampperframe.counts/sampperframe.dt
+
+    frameStartIndx = np.hstack((0,np.where(tsDf.framestart)[0]))
+    frameStartIndx = np.hstack((frameStartIndx, frameStartIndx[-1]+sampperframe.counts.values[-1]-1))
+    frameIndx = tsDf.index.values
+
+    frameNums = tsDf.frame[frameStartIndx].values.astype('int')
+    frameNumsInterp = np.hstack((frameNums, frameNums[-1]+1))
+
+    timeAtFramestart = tsDf.time[frameStartIndx].values
+
+    #generate interpolated frames
+    frameinterp_f = interpolate.interp1d(frameStartIndx,frameNums)
+    tsDf['frameinterp'] = frameinterp_f(frameIndx)
+
+    timeinterp_f = interpolate.interp1d(frameStartIndx,timeAtFramestart)
+    tsDf['timeinterp'] = timeinterp_f(frameIndx)
+    
+    return tsDf
