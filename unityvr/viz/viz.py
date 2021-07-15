42f41ef1d7d3ad5409eb635ca3157c79136998b3
hjmh <hjmhaberkern@gmail.com>
2021-07-14 23:21:00 -0400

    expand existing plotting functions, add path plot

--- unityvr/viz/viz.py
+++ unityvr/viz/viz.py
@@ -18,7 +18,7 @@ def myAxisTheme(myax):
 def plotScaleBar(ax,xlen,pos,labeltext):
     ax.plot([pos[0],pos[0]+xlen],[pos[1],pos[1]],'k')
     ax.text(pos[0],pos[1],labeltext)
-    
+
 def minimalAxisTheme(myax, xlen,pos,labeltext):
     plotScaleBar(myax,xlen,pos,labeltext)
     myax.axis('off')
@@ -34,33 +34,43 @@ def pathPlotAxisTheme(myax, units):
     myax.set_aspect('equal')
     myax.set_xlabel('x [{}]'.format(units))
     myax.set_ylabel('y [{}]'.format(units))
-    
-    
-    
+
+def plotFlyPath(uvrTest, convfac, figsize):
+    fig, axs = plt.subplots(1,2,figsize=figsize, gridspec_kw={'width_ratios':[20,1]})
+    axs[0].plot(uvrTest.posDf.x*convfac,uvrTest.posDf.y*convfac,color='grey', linewidth=0.5)
+    cb = axs[0].scatter(uvrTest.posDf.x*convfac,uvrTest.posDf.y*convfac,s=5,c=uvrTest.posDf.angle, cmap='hsv')
+    axs[0].plot(uvrTest.posDf.x[0]*convfac,uvrTest.posDf.y[0]*convfac,'ok')
+    axs[0].text(uvrTest.posDf.x[0]*convfac+0.2,uvrTest.posDf.y[0]*convfac+0.2,'start')
+    axs[0].plot(uvrTest.posDf.x.values[-1]*convfac,uvrTest.posDf.y.values[-2]*convfac,'sk')
+    plt.colorbar(cb,cax=axs[1], label='head direction [degree]')
+
+    return fig, axs
+
+
 def plotVRpathWithObjects(uvrExperiment,limx,limy, myfigsize):
 
     fig, ax = plt.subplots(1,1, figsize=myfigsize)
-    
+
     ax = plotAllObjects(uvrExperiment, ax)
 
     ax.plot(uvrExperiment.posDf['x'], uvrExperiment.posDf['y'],color='grey',alpha=0.5)
     ax.scatter(uvrExperiment.posDf['x'], uvrExperiment.posDf['y'],s=7,c=uvrExperiment.posDf['time'],cmap='viridis')
-    
+
     if np.isfinite(limx[0]):
         ax.set_xlim(limx[0], limx[1])
         ax.set_ylim(limy[0], limy[1])
     ax.set_aspect('equal')
-    
-    return fig
+
+    return fig, ax
 
 def plotAllObjects(uvrExperiment, ax):
-    
+
     for obj in range(uvrExperiment.objDf.shape[0]):
         if("Plane" in uvrExperiment.objDf.name[obj]): continue
-            
+
         if("FlyCamera" not in uvrExperiment.objDf.name[obj]):
-            ax = plotObjectEllipse(ax, 
-                                   [uvrExperiment.objDf['sx'][obj], uvrExperiment.objDf['sy'][obj]], 
+            ax = plotObjectEllipse(ax,
+                                   [uvrExperiment.objDf['sx'][obj], uvrExperiment.objDf['sy'][obj]],
                                    [uvrExperiment.objDf['px'][obj], uvrExperiment.objDf['py'][obj]])
             ax.annotate(uvrExperiment.objDf['name'][obj], (uvrExperiment.objDf['px'][obj]+5, uvrExperiment.objDf['py'][obj]-10))
     return ax
@@ -69,6 +79,5 @@ def plotAllObjects(uvrExperiment, ax):
 def plotObjectEllipse(ax, rad, pos):
     ellipse = mpatches.Ellipse((pos[0],pos[1]), rad[0], rad[1], color='grey', alpha=0.5)
     ax.add_patch(ellipse)
-    
-    return ax
 
+    return ax
