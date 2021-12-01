5b3e8125999c40888cc83d694b2d5ed5326515b0
hjmh <hjmhaberkern@gmail.com>
2021-12-01 11:45:37 -0500

    Update preprocessUnityLog.py

--- unityvr/scripts/preprocessUnityLog.py
+++ unityvr/scripts/preprocessUnityLog.py
@@ -1,6 +1,10 @@
 # Batch processing function to preprocess unity logs from a single fly experiment
 
 def preprocessUnityLogs(rootDir, dataDir):
+    #Example:
+    #rootDir = '/Volumes/jayaramanlab/Hannah/Projects/FlyVR2P/Data/'
+    #dataDir = 'disappearingSun/SS96_x_7f/EB/f04'
+
     import numpy as np
     import matplotlib.pyplot as plt
     from os import listdir
@@ -8,16 +12,13 @@ def preprocessUnityLogs(rootDir, dataDir):
 
     from unityvr.preproc import logproc as lp
 
-    #rootDir = '/Volumes/jayaramanlab/Hannah/Projects/FlyVR2P/Data/'
-    #dataDir = 'disappearingSun/SS96_x_7f/EB/f04'
-
     dirName = rootDir + 'raw/' + dataDir
     condition = datPath.split('/')[3]
     preprocDir = rootDir + 'preproc/'+ dataDir
 
     fileNames = [f for f in listdir(dirName) if isfile(join(dirName, f)) and '.json' in f]
     print(fileNames)
-    
+
     for fileName in fileNames:
     uvrTrial = lp.constructUnityVRexperiment(dirName,fileName)
     uvrTrial.printMetadata()
