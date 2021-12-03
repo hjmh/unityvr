59c2d3f5c1c1d385df33eef8362b2f882673f01a
shivChitinous <shivamchitnis66@gmail.com>
2021-12-02 21:41:23 -0500

    shift to mixture of vmf instead of generalized vmf

--- unityvr/analysis/fittingFunctions.py
+++ unityvr/analysis/fittingFunctions.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from unityvr.analysis.utilityFunctions import getTrajFigName
 from unityvr.viz import viz
 
-from scipy.special import i0
+from scipy.special import i0, iv
 from scipy.optimize import curve_fit
 import scipy.stats as sts
 
@@ -19,7 +19,9 @@ def vonmises_pdf(x, mu, kappa):
     return V
 
 def sum_of_vonmises_pdf(x, mu1, kappa1, mu2, kappa2):
-    V = np.exp((kappa1)*np.cos((x-mu1)))/(2*np.pi*i0(kappa1)) + np.exp((kappa2)*np.cos((x-mu2)))/(2*np.pi*i0(kappa2))
+    #mixture of vonmises function with 4 parameters
+    V = 0.5*(vonmises_pdf(x, mu1, kappa1)+vonmises_pdf(x, mu2, kappa2))
+    return V
 
 #function to fit data to the von mises pdf
 def fit_vonmises(degAngles, binwidth = 20, plot = False, plotsave=False, saveDir=None, uvrDat=None):
@@ -37,6 +39,8 @@ def fit_vonmises(degAngles, binwidth = 20, plot = False, plotsave=False, saveDir
     #get probability density and theta vector
     theta = np.linspace(0,2*np.pi,num=numbins+1)[:-1] + binwidth/2
     p = np.histogram(angles,bins=numbins,density=True)[0]
+    
+    headingPVAmag = np.abs(np.nanmean(np.exp(1j*theta)))
 
     plt.figure(figsize = (9,2))
     plt.step(theta*180/np.pi, p)
@@ -52,9 +56,12 @@ def fit_vonmises(degAngles, binwidth = 20, plot = False, plotsave=False, saveDir
     sqd = np.sum(np.square(p-fit_func))
     
     #decide whether distribution is unimodal
-    notFit = np.logical_and(np.logical_and(p_value<=0.1, params[1]<0.3),~np.isnan(params[1]))
+    notFit = np.all([p_value<=0.1, headingPVAmag<0.5])
     
-    #if not:
+    #create a neitherFit variable
+    neitherFit = False
+    
+    #if not unimodal:
     #fit p as a function of theta to a sum of vonmises
     if notFit:
         params, _ = curve_fit(sum_of_vonmises_pdf, theta, p, bounds=([0,0,0,0],[2*np.pi,np.inf,2*np.pi,np.inf]))
@@ -65,38 +72,58 @@ def fit_vonmises(degAngles, binwidth = 20, plot = False, plotsave=False, saveDir
 
         #compute squared difference from fit
         sqd = np.sum(np.square(p-fit_func))
-    
-    if plot:
-        if notFit: V = sum_of_vonmises_pdf(np.linspace(0,2*np.pi,num=50), params[0], params[1], params[2], params[3])
-        else: V = vonmises_pdf(np.linspace(0,2*np.pi,num=50), params[0], params[1])
         
-        #plot 1
-        plt.plot(np.linspace(0,360,num=50), V, 'k-')
-        plt.xlabel(r"$\theta$")
-        
-        if plotsave:
-            plt.savefig(getTrajFigName("fit_vonmises",saveDir,uvrDat.metadata))
-        
-        #plot 2
-        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-        if notFit: 
-            ax.plot(params[0], params[1], 'ro', alpha=0.5)
-            ax.plot(params[2], params[3], 'bo', alpha=0.5)
-        else: ax.plot(params[0], params[1], 'ro', alpha=0.5)
-        ax.set_yticks([0.5,1])
-        ax.set_theta_zero_location("E")
-        ax.set_xticks(np.pi/180 * np.arange(-180,  180,  45))
-        ax.set_thetalim(-np.pi, np.pi);
-
-        if plotsave:
-            fig.savefig(getTrajFigName("mu_kappa",saveDir,uvrDat.metadata))
-
-        #returns mu in degree
+        #decide whether distribution is bimodal:
+        neitherFit = p_value<=0.1
     
-    mu1 = params[0]*180/np.pi
-    kappa1 = params[1]
-    mu2 = params[2]*180/np.pi if notFit else None
-    kappa2 = params[3] if notFit else None
+    if neitherFit:
+        print("Neither unimodal nor bimodal fit.")
+        mu1 = float("NaN")
+        kappa1 = float("NaN")
+        mu2 =  float("NaN")
+        kappa2 = float("NaN")
+        p_value =  float("NaN")
+        sqd = float("NaN")
     
-    return mu1, kappa1, mu2, kappa2, ks, p_value, sqd
+    else:
+        if notFit:
+            mus = np.array([params[0],params[2]])
+            kappas = np.array([params[1],params[3]])
+            kappa1 = np.max(kappas); kappa2=np.min(kappas)
+            mu1 = mus[np.argmax(kappas)]; mu2 = mus[np.argmin(kappas)]
 
+        else:
+            mu1 = params[0]
+            kappa1 = params[1]
+            mu2 = None
+            kappa2 = None
+        
+        if plot:
+            if notFit: V = sum_of_vonmises_pdf(np.linspace(0,2*np.pi,num=50), mu1, kappa1, mu2, kappa2)
+            else: V = vonmises_pdf(np.linspace(0,2*np.pi,num=50), mu1, kappa1)
+
+            #plot 1
+            plt.plot(np.linspace(0,360,num=50), V, 'k-')
+            plt.xlabel(r"$\theta$")
+
+            if plotsave:
+                plt.savefig(getTrajFigName("fit_vonmises",saveDir,uvrDat.metadata))
+
+            #plot 2
+            fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+            ax.plot(mu1, kappa1, 'ro', alpha=0.5)
+            if notFit: 
+                ax.plot(mu2, kappa2, 'bo', alpha=0.5)
+            ax.set_yticks([0.5,1])
+            ax.set_theta_zero_location("E")
+            ax.set_xticks(np.pi/180 * np.arange(-180,  180,  45))
+            ax.set_thetalim(-np.pi, np.pi);
+
+            if plotsave:
+                fig.savefig(getTrajFigName("mu_kappa",saveDir,uvrDat.metadata))
+                
+            #returns mu in degree
+       
+    return mu1*180/np.pi, kappa1, mu2*180/np.pi if mu2 is not None else mu2, kappa2, ks, p_value, sqd
+    
+    
