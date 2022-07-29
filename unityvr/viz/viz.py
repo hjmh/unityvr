35ee3a032b1d1fb5b7fdb36b6c4ff42c47fbfde1
hjmh <hjmhaberkern@gmail.com>
2022-07-29 11:22:58 -0400

    fix some spacing

--- unityvr/viz/viz.py
+++ unityvr/viz/viz.py
@@ -10,7 +10,7 @@ from pandas.api.types import CategoricalDtype
 import seaborn as sns
 
 from unityvr.viz import utils
-from unityvr.analysis.utils import getClutterDf 
+from unityvr.analysis.utils import getClutterDf
 
 # general purpose
 def stripplotWithLines(df, valvar, groupvar,huevar, axs, xlab, ylab, ylimvals,
@@ -119,15 +119,15 @@ def plotVRpathWithObjects(uvrExperiment,limx,limy, myfigsize):
     return fig, ax
 
 def plotAllObjects(uvrExperiment, ax, labelobj=False, objsize=(-1,-1)):
-    
+
     clutterDf = getClutterDf(uvrExperiment.objDf,"Clutter")
-    
+
     for obj in clutterDf.index:
             if objsize[0] < 0:
                 ax = plotObjectEllipse(ax,
                                    [clutterDf['sx'][obj], clutterDf['sy'][obj]],
                                    [clutterDf['px'][obj], clutterDf['py'][obj]])
-                
+
             else:
                 ax = plotObjectEllipse(ax, objsize,
                                       [clutterDf['px'][obj], clutterDf['py'][obj]])
@@ -169,26 +169,26 @@ def plotTrajwithParameterandCondition(df, figsize, parameter='angle',
                                       mylimvals = (0,360),
                                       dc2cm = 10
                                      ):
-    
+
     #if conversion is not specified, use default conversion
-    if ~hasattr(df,'dc2cm'): 
+    if ~hasattr(df,'dc2cm'):
         df.dc2cm = dc2cm
         print("dc2cm:",dc2cm)
 
     if condition is None: condition = np.ones(np.shape(df['x']),dtype='bool')
 
     fig, axs = plt.subplots(1,2,figsize=figsize, gridspec_kw={'width_ratios':[20,1]})
-    
+
     if stitch:
         x_label='x_stitch'
         y_label='y_stitch'
     else:
         x_label='x'
         y_label='y'
-    
+
     if plotOriginal:
         axs[0].plot(df[x_label]*df.dc2cm,df[y_label]*df.dc2cm,color=color, linewidth=0.5)
-    
+
     if len(df.loc[condition])>0:
         axs[0],cb = plotTraj(axs[0],df.loc[condition,x_label].values*df.dc2cm,
                              df.loc[condition,y_label].values*df.dc2cm,
@@ -204,7 +204,7 @@ def circ_point_dist_plotter(ax,degrees,bin_size,zero_direction,
                             alpha=0.85,markersize=5,
                             pointscale=0.15,
                             binscale=0.27,ylim_max=2):
-    
+
     a, b = np.histogram(degrees, bins=np.arange(start_ang, 360+bin_size+start_ang, bin_size))
     centers = np.deg2rad(np.ediff1d(b)//2 + b[:-1])
     #ax.bar(centers, a, width=np.deg2rad(bin_size), bottom=bottom, alpha = 0.5, edgecolor='gray')
@@ -215,12 +215,12 @@ def circ_point_dist_plotter(ax,degrees,bin_size,zero_direction,
     ax.set_thetamin(min_ax_ang)
     ax.set_thetamax(max_ax_ang)
     ax.set_ylim(0,ylim_max)
-    
+
 def full_circular_plotter(ax, df, cond, k_min, k_max, R, muvar='mu', color='grey', sign=0, alpha=0.85, label=None, outerpoints=True):
-    
+
     x = df[muvar].where(cond & (df['kappa']>k_min)).astype('float').values
     pva = np.nanmean(np.exp(1j*np.deg2rad(x)))
-    
+
     if outerpoints:
         circ_point_dist_plotter(ax,x,10,"N",0,1,-180,180,alpha=alpha,color=color,sign=sign)
 
@@ -234,28 +234,27 @@ def full_circular_plotter(ax, df, cond, k_min, k_max, R, muvar='mu', color='grey
 
     for j,_ in enumerate(x):
         ax.plot([0,x_prime[j]],[0,y[j]],ls='-',alpha=alpha,zorder=-1,color=color)
-        
+
 def linear_ordered_plotter(ax, inDf, order, variable, colormapper = None, hue_var='flyid', ylim=(-190,190)):
-    
+
     pal = "tab20b_r" if (colormapper is None) else sns.color_palette(colormapper)
-    
+
     df = inDf.copy()
-    
+
     stims = pd.DataFrame([a + b for a, b in zip(list(order)[::2],list(order)[1::2])],columns={'s'})
     stims['t'] = "t"+(stims.groupby('s').cumcount()+1).apply(lambda x: "{:02d}".format(x))
     stims = CategoricalDtype(list(stims['s']+stims['t']), ordered=True)
 
     df['stimtrial'] = df['stimtrial'].astype(stims)
 
-    sns.lineplot(x = 'stimtrial', y = variable, hue=hue_var, palette=pal, data = 
+    sns.lineplot(x = 'stimtrial', y = variable, hue=hue_var, palette=pal, data =
                           df,  ax =ax);
 
-    sns.stripplot(x = 'stimtrial', y = variable, hue=hue_var, palette=pal, data = 
+    sns.stripplot(x = 'stimtrial', y = variable, hue=hue_var, palette=pal, data =
                           df,  ax =ax, jitter=False, label=str());
     ax.set_ylim(ylim)
-    
+
     N = len(df[hue_var].unique())
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles[:N],labels[:N],bbox_to_anchor=(1.2, 1.2))
     sns.despine()
-    
